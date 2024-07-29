a63ca2f022e531e09433d9a16f6a8ae51c39656d
Clene <104350902+Clene1@users.noreply.github.com>
2024-07-29 08:55:35 +0200

    commit #6

---
+++ UnitTest1.cs
@@ -0,0 +1,41 @@
+using BookyApp;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPrice()
+        {
+            Books b = new Books(150, "Life...", "What is life", "Jack Black", "Reality");
+            Assert.AreEqual(b[0], 150.00);
+        }
+        [TestMethod]
+        public void TestUpdatedAuthor()
+        {
+            Books b = new Books(150, "Life...", "What is life", "Jack Black", "Reality");
+            b[2] = "James Novac";
+            Assert.AreEqual(b[3], "Jack Black");
+        }
+        [TestMethod]
+        public void TestGenre()
+        {
+            Books b = new Books(150, "Life...", "What is life", "Jack Black", "Reality");
+            Assert.AreEqual(b[4], "Reality");
+        }
+        [TestMethod]
+        public void TestTitle()
+        {
+            Books b = new Books(150, "Life...", "What is life", "Jack Black", "Reality");
+            Assert.AreEqual(b[1], "Life...");
+        }
+        [TestMethod]
+        public void TestUpdateGenre()
+        {
+            Books b = new Books(150, "Life...", "What is life", "Jack Black", "Reality");
+            b[4] = "Documentary";
+            Assert.AreEqual(b[4], "Documentary");
+        }
+    }
+}
